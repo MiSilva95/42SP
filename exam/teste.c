37959f6589e10aee4534aaaf988387d2f01c66dd
Michele Da Silva <mida-sil@c2r10p1.42sp.org.br>
2025-06-14 11:01:36 -0300

    exam

--- exam/teste.c
+++ exam/teste.c
@@ -1,21 +1,19 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    int i = 0;
-    if(ac == 2)
-    {
-        while(av[1][i])
-        {
-            if(av[1][i] >= 'A' && av[1][i] <= 'Z')
-            {
-                write(1, "_", 1);
-                av[1][i] += 32;
-            }
-            write(1, &av[1][i], 1);
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return 0;
+	if (argc == 2)
+	{
+		for (int i = 0; argv[1][i]; i += 1)
+		{
+			if (argv[1][i] == '_')
+			{
+				i += 1;
+				argv[1][i] -= 32;
+			}
+			write(1, &argv[1][i], 1);
+		}
+	}
+	write(1, "\n", 1);
+	return 0;
 }
