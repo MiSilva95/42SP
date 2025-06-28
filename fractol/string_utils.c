1d1ebaccf69471a2cf87ce41c842ab55a9115104
Michele Da Silva <mida-sil@c2r12p6.42sp.org.br>
2025-06-28 13:29:31 -0300

    fractol

---
+++ fractol/string_utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mida-sil <mida-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 09:17:34 by mida-sil          #+#    #+#             */
+/*   Updated: 2025/06/28 13:27:05 by mida-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+
+int	ft_strncmp(char *s1, char *s2, int n)
+{
+	if (NULL == s1 || NULL == s2 || n <= 0)
+		return (0);
+	while (*s1 == *s2 && n > 0 && *s1 != '\0')
+	{
+		++s1;
+		++s2;
+		--n;
+	}
+	// chars are basically 1 byte int
+	// 🚨 when 0, strings are equal !! 🚨
+	return (*s1 - *s2);
+}
+
+/*
+ * RECURSION
+*/
+void	putstr_fd(char *s, int fd)
+{
+	if (NULL == s || fd < 0)
+		return ;
+	if (*s != '\0')
+	{
+		write(fd, s, 1);
+		putstr_fd(s + 1, fd);
+	}
+}
+
+/*
+ * ALPHA TO DOUBLE
+ * similar to atoi, but dealing with floats
+ * takes the cmnd line args and
+ * converts to long double (typedef ldbl)
+*/
+double	atodbl(char *s)
+{
+	long	integer_part;
+	double	fractional_part;
+	double	pow;
+	int		sign;
+
+	integer_part = 0;
+	fractional_part = 0;
+	sign = +1;
+	pow = 1;
+	while ((*s >= 9 && *s <= 13) || 32 == *s)
+		++s;
+	while ('+' == *s || '-' == *s)
+		if ('-' == *s++)
+			sign = -sign;
+	while (*s != '.' && *s)
+		integer_part = (integer_part * 10) + (*s++ - 48);
+	if ('.' == *s)
+		++s;
+	while (*s)
+	{
+		pow /= 10;
+		fractional_part = fractional_part + (*s++ - 48) * pow;
+	}
+	return ((integer_part + fractional_part) * sign);
+}
